02ce53a089fca219b89841c59277a1d60ee69b81
Kryllee <k.laderaaa@gmail.com>
2025-04-10 12:05:14 +0800

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
     class User extends Model{
         protected $table = 'tbluser';
-        // column sa table
-        protected $fillable = ['username', 'password','gender'];
+
+        protected $fillable = ['username', 'password','gender', 'jobid'];
 
         public $timestamps = false;
+
         protected $primaryKey = 'id';
+
+        protected $hidden =['password'];
  }
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,7 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->put('/users/{id}', 'UserController@update'); // Update user
     $router->patch('/users/{id}', 'UserController@update'); // Partial update
     $router->delete('/users/{id}', 'UserController@delete'); // Delete user
+
+    $router->get('/usersjob/', 'UserJobController@index'); // Get all job
+    $router->get('/usersjob/{id}', 'UserJobController@show'); // Get user by ID
 });
